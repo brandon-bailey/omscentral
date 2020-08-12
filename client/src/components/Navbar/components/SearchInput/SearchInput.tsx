399101f70eca1f105943a92ba611b1598e64cdca
Mehmet Bajin <bajin.mehmet@gmail.com>
2020-08-12 16:02:33 -0400

    logs(search): track search analytics

--- client/src/components/Navbar/components/SearchInput/SearchInput.tsx
+++ client/src/components/Navbar/components/SearchInput/SearchInput.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 
+import { FirebaseContext } from 'src/components/Firebase';
 import { useStyles } from './SearchInput.styles';
 
 interface Props {
@@ -12,6 +13,7 @@ interface Props {
 
 const SearchInput: React.FC<Props> = ({ value, onChange, onSubmit }) => {
   const classes = useStyles();
+  const firebase = useContext(FirebaseContext);
 
   return (
     <div className={classes.search}>
@@ -27,7 +29,12 @@ const SearchInput: React.FC<Props> = ({ value, onChange, onSubmit }) => {
         inputProps={{ 'aria-label': 'search' }}
         value={value}
         onChange={(event) => onChange(event.target.value)}
-        onKeyDown={(event) => event.key === 'Enter' && onSubmit(value)}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter') {
+            firebase.analytics.logEvent('search', { search_term: value });
+            onSubmit(value);
+          }
+        }}
       />
     </div>
   );
