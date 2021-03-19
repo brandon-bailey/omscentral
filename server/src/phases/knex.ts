3217591bf1464d2e6499ca2267f09d8092b01016
Mehmet Bajin <bajin.mehmet@gmail.com>
2021-03-19 09:13:47 -0400

    fix(server): no ssl

--- server/src/phases/knex.ts
+++ server/src/phases/knex.ts
@@ -2,12 +2,21 @@ import knex, { Config } from 'knex';
 import { Model } from 'objection';
 
 import { PhaseFunction } from '../components';
-import { postgresConfig } from '../config';
+import { appConfig, postgresConfig } from '../config';
 
 export const phase: PhaseFunction = (app, next) => {
+  const ssl =
+    appConfig.environment === 'staging' ||
+    appConfig.environment === 'production';
+
+  const queryString = ssl ? 'sslmode=require' : '';
+
   const knexConfig: Config = {
     client: 'pg',
-    connection: postgresConfig.connection,
+    connection: {
+      connectionString: `${postgresConfig.connection}?${queryString}`,
+      ssl,
+    },
   };
 
   Model.knex(knex(knexConfig));
